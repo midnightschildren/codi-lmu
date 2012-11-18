f348be4c2163eb8db07bb2f4588320fb2400058f
midnightschildren <amar@codisattva.com>
2012-11-18 02:38:43 -0800

    Removal of extra fonts

--- header.php
+++ header.php
@@ -12,7 +12,6 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>><head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
 <?php if (is_search()) { ?>
 <meta name="robots" content="noindex, nofollow" />
 <?php } ?>
